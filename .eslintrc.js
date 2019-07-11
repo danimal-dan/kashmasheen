0ad04c99a4234a75e7fc54427698e582391326c9
Daniel Collins <danielc111@gmail.com>
2019-07-10 23:55:42 -0400

    Setup project and add logos

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,35 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/prettier'],
+  plugins: ['prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': 'off',
+    'max-len': [2, 140, 4],
+    'prettier/prettier': 'error',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: true,
+        ignores: ['/^el-/'],
+      },
+    ],
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js'],
+      },
+    },
+  },
 };
